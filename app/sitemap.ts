cec867e49fef6f3f005df58e18dc40f3dae219d8
ruos-sovanra <sovanraruos@gmail.com>
2024-03-30 22:31:38 +0700

    Add read me

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,25 @@ import {MetadataRoute} from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://nextjs-typescript-eta.vercel.app',
+            url: 'http://128.199.127.236:3001/',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
         },
         {
-            url: 'https://nextjs-typescript-eta.vercel.app/about',
+            url: 'http://128.199.127.236:3001/product',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'https://nextjs-typescript-eta.vercel.app/service',
+            url: 'http://128.199.127.236:3001/about',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'http://128.199.127.236:3001/contact',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.5,
